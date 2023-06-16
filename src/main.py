a58a37b70d935d937486160ad8bc7befea6bdf83
my <jakha921@gmail.com>
2023-06-16 16:45:07 +0500

    add cors

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, status
 from fastapi.exceptions import ValidationError
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.roles.routers import router as roles_routes
 from src.countries.routers import router as countries_routes
@@ -13,12 +14,29 @@ from src.statuses.routers import router as statuses_routes
 from src.complain_statuses.routers import router as complain_statuses_routes
 from src.departments.routers import router as department_routes
 from src.categories.routers import router as categories_routes
-
+from src.rights.routers import router as rights_routes
+from src.users.routers import router as users_routes
 
 app = FastAPI(
     title="Agriculture API",
     description="API for agriculture",
     version="0.0.1",
+)
+
+origins = [
+    "*"
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    # "http://localhost",
+    # "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -44,5 +62,7 @@ app.include_router(districts_routes)
 app.include_router(department_routes)
 app.include_router(genders_routes)
 app.include_router(statuses_routes)
+app.include_router(users_routes)
 app.include_router(complain_statuses_routes)
 app.include_router(categories_routes)
+app.include_router(rights_routes)
