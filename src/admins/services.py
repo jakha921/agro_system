2883769e5c6781b163585035390d65ce86e61efb
my <jakha921@gmail.com>
2023-07-04 09:41:09 +0500

    add to admin roles

--- src/admins/services.py
+++ src/admins/services.py
@@ -85,6 +85,41 @@ class AdminService(BaseService):
                 "data": str(e) if str(e) else None
             })
 
+    async def get_entity(self, entity_id: int, session: AsyncSession):
+        """
+        Get entity by id
+        """
+        try:
+            query = select(self.model)
+
+            query = query.options(joinedload(self.model.role))
+
+            query = query.where(self.model.id == entity_id)
+            entity = (await session.execute(query)).scalars().first()
+
+            # exclude password field
+            entity.password = None
+
+            if entity is None:
+                raise HTTPException(status_code=404, detail=f"{self.get_entity_name()} not found")
+            return {
+                "status": "success",
+                "detail": f"{self.get_entity_name()} retrieved successfully",
+                "data": entity
+            }
+        except HTTPException as e:
+            raise HTTPException(status_code=404, detail={
+                "status": "error",
+                "detail": e.detail,
+                "data": None
+            })
+        except Exception as e:
+            raise HTTPException(status_code=400, detail={
+                "status": "error",
+                "detail": f"{self.get_entity_name()} not retrieved",
+                "data": str(e) if str(e) else None
+            })
+
     async def create_entity(self, entity_data, session: AsyncSession):
         """
         Create entity
@@ -127,7 +162,8 @@ class AdminService(BaseService):
         """
         try:
             get_entity = await self.get_entity_by_email(entity_data.email, session)
-            if get_entity["status"] == "success" and get_entity["data"] is not None:
+            if get_entity["status"] == "success" and get_entity["data"] is not None and get_entity[
+                "data"].id != entity_id:
                 raise HTTPException(status_code=400, detail=f"{self.get_entity_name()} already exists")
 
             query = select(self.model).where(self.model.id == entity_id)
