691c7cd415b99ad8f4e0535d10797178ab5e0312
my <jakha921@gmail.com>
2023-07-06 14:08:50 +0500

    fix bug with date

--- src/complains/schemas.py
+++ src/complains/schemas.py
@@ -11,7 +11,7 @@ class ComplainCreate(BaseModel):
     description: str = Field(..., max_length=255)
     image: str = Field(..., max_length=255)
     rate: int = Field(..., ge=0, le=5)
-    complain_date: datetime = Field(...)
+    action_date: datetime = Field(...)
 
     class Config:
         orm_mode = True
