04be9bbff72005c6f29d3dab672e948d69607ef8
my <jakha921@gmail.com>
2023-07-08 11:03:20 +0500

    add to regions country

--- src/rigions/services.py
+++ src/rigions/services.py
@@ -6,3 +6,6 @@ from src.base_service.base_service import BaseService
 class RegionService(BaseService):
     def get_entity_name(self):
         return "Region"
+
+    def get_addition_entity_name(self):
+        return joinedload(self.model.country)
