a0dcfd5ae8ed6e857ace22eef8ac7673a34bf26b
my <jakha921@gmail.com>
2023-08-04 01:41:25 +0500

    change permission for user

--- src/users/routers.py
+++ src/users/routers.py
@@ -20,35 +20,40 @@ users_service = UserService(User)
 @router.get("/")
 async def get_users(page: int = None, limit: int = None, search: str = None,
                     session: AsyncSession = Depends(get_async_session),
-                    current_user: str = Depends(JWTBearer())):
-    check_permission("read_user", current_user)
+                    # current_user: str = Depends(JWTBearer())
+                    ):
+    # check_permission("read_user", current_user)
     return await users_service.get_entities(session, page, limit, search)
 
 
 @router.post("/")
 async def create_user(user: schemas.UserCreate, session: AsyncSession = Depends(get_async_session),
-                      current_user: str = Depends(JWTBearer())):
-    check_permission("create_user", current_user)
+                      # current_user: str = Depends(JWTBearer())
+                      ):
+    # check_permission("create_user", current_user)
     return await users_service.create_entity(user, session)
 
 
 @router.get("/{user_id}")
 async def get_user(user_id: int, session: AsyncSession = Depends(get_async_session),
-                   current_user: str = Depends(JWTBearer())):
-    check_permission("read_user", current_user)
+                   # current_user: str = Depends(JWTBearer())
+                   ):
+    # check_permission("read_user", current_user)
     return await users_service.get_entity(user_id, session)
 
 
 @router.patch("/{user_id}")
 async def update_user(user_id: int, user: schemas.UserUpdate,
                       session: AsyncSession = Depends(get_async_session),
-                      current_user: str = Depends(JWTBearer())):
-    check_permission("update_user", current_user)
+                      # current_user: str = Depends(JWTBearer())
+                      ):
+    # check_permission("update_user", current_user)
     return await users_service.update_entity(user_id, user, session)
 
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: int, session: AsyncSession = Depends(get_async_session),
-                      current_user: str = Depends(JWTBearer())):
-    check_permission("delete_user", current_user)
+                      # current_user: str = Depends(JWTBearer())
+                      ):
+    # check_permission("delete_user", current_user)
     return await users_service.delete_entity(user_id, session)
